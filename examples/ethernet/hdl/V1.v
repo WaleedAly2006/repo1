e6ba90c79b6b9ce0cfd84bbb713a578b75a36143
Waleed_Aly <waleed_aly@mentor.com>
2020-05-13 15:56:56 +0200

    add new file

---
+++ examples/ethernet/hdl/V1.v
@@ -0,0 +1,18 @@
+//
+// Verilog Module Ethernet.V1
+//
+// Created:
+//          by - waly.UNKNOWN (EGC-WALY-LT)
+//          at - 02:40:29 05/13/2020
+//
+// using Mentor Graphics HDL Designer(TM) 2020.2 Built on 2 Mar 2020 at 18:07:13
+//
+
+`resetall
+`timescale 1ns/10ps
+module V1 ;
+
+
+// ### Please start your Verilog code here ### 
+
+endmodule
